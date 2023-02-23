e3d74e781fcd29cc268f47155060812150511000
britneyhu <111310141+britneyhu@users.noreply.github.com>
2023-02-22 16:22:43 -0800

    Update settings.py

--- movie/settings.py
+++ movie/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0fhc28fj1xfdge(n@zbuyy3oe%qsgps4hg297zg#gx03^o#_rf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
