99e0fae1f987ab3ad90b995b1ee2c1d7996f9282
britneyhu <111310141+britneyhu@users.noreply.github.com>
2023-02-22 16:45:01 -0800

    Update settings.py

--- movie/settings.py
+++ movie/settings.py
@@ -142,4 +142,4 @@ STATICFILES_DIRS = [
      os.path.join(BASE_DIR, 'static')
  ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = '/home/ubuntu/django/django_movie_project/static'
