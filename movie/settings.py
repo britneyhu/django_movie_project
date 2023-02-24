8b69d3d305b9bcde31e4cfae3f8ab0c303e39e11
britneyhu <111310141+britneyhu@users.noreply.github.com>
2023-02-23 18:27:58 -0800

    Update settings.py

--- movie/settings.py
+++ movie/settings.py
@@ -139,7 +139,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, ''),
  ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "/staticfiles/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
